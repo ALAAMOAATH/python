c959ba0c66df72a79d5ead06ef13058b5c309c90
Alaa Moaath <alaamoaath3@gmail.com>
2023-09-12 03:31:27 +0300

    learn

--- Study/alaa.py
+++ Study/alaa.py
@@ -1,32 +1,3 @@
-# cell=2
-# result=0
-# # with sum before
-# for day in range(1,31):
-#     result+=cell
-#     cell=cell*2
-#     print(f"in day {day} =    {result}") 
+a = lambda x , y : x+y
 
-# print(result)    
-# sum=0
-# for day in range(1,31):
-#     result=cell
-#     sum+=cell
-#     cell=cell*2
-#     print(f"in day {day} =    {result}") 
-
-  
-# print(sum)
-
-# bubble sort
-
-array=[2,3,4,6,7,8,0,10,3,4,6,7,8,3,1,0]
-min=array[0]
-for i in range(len(array)):
-    for j in range(i+1,len(array)):
-        if(array[j]>array[i]):
-            min=array[j]
-            array[j]=array[i]
-            array[i]=min
-
-
-print(array)
+print(a(1,2))
--- Study/h_w.py
+++ Study/h_w.py
@@ -1,2 +1,10 @@
-test =[]
+c = 9 
+def test():
+    global c
+    c=88
+    return c
+
+print(c)
+test()
+print(c)
 
